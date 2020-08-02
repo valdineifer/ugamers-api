5aad79019a13cbb51b90cf3e9cc5f77d8a31d7a3
Valdinei Ferreira <valdineifer.dev@outlook.com>
2020-08-02 18:27:45 -0300

    add API files with auth and docker files

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # uGamers
-by Daniele Valverde e Valdinei Ferreira
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
+
+---
+
+by Daniele Valverde and Valdinei Ferreira
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
-import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { mailerConfig } from './configs/mailer.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration]
-    })
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    MailerModule.forRoot(mailerConfig),
+    UsersModule,
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
