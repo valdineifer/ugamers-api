1d90cdaf32e544c583dc4321062048e8586ee442
Valdinei Ferreira <valdineifer@outlook.com>
2021-03-13 03:00:58 -0300

    chore(seeding): add seed packages and refactor seed generator
    - now generate as PascalCase, instead of snake_case

--- src/commands/generate-seed.ts
+++ src/commands/generate-seed.ts
@@ -1,21 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
-const pascalToSnakeCase = (s: string) => {
-  const str = s.charAt(0).toLowerCase() + s.slice(1);
-  return str.replace(/[A-Z]/g, (letter: string) => `_${letter.toLowerCase()}`);
-};
-
-const capitalize = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 
 const fileTemplate = (seedName: string) =>
   `import { Factory, Seeder } from 'typeorm-seeding';
 
 export default class ${capitalize(seedName)} implements Seeder {
   public async run(factory: Factory): Promise<any> {
-    // await factory(Entity)().createMany(10);
+    // await factory(Entity)().create();
   }
 }
 `;
@@ -23,7 +16,7 @@ export default class ${capitalize(seedName)} implements Seeder {
 function generateSeed(seedName: string) {
   const time = Date.now();
 
-  const fileName = `${time}-${pascalToSnakeCase(seedName)}`;
+  const fileName = `${time}-${capitalize(seedName)}`;
 
   const seedPath = path.resolve(__dirname, '..', 'database', 'seeds');
 
